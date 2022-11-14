4f72ea571ea7ebe18df12dbb5287ac73dc4e20da
Konstantin Aleksandrov <kostyagusto@gmail.com>
2022-11-14 22:36:42 +0200

    added fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
 
-function App() {
+class App extends Component() {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
+  render(){
   return (
     <div className="App">
       <header className="App-header">
@@ -17,9 +33,11 @@ function App() {
         >
           Learn React
         </a>
+        <p className="App-intro">;{this.state.apiResponse}</p>
       </header>
     </div>
   );
+  }
 }
 
 export default App;
